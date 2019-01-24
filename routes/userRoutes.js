9ade86a1262e276c801a970e4d27370856a6072d
Magda Sidor <magda_si@wp.pl>
2019-01-24 15:58:31 +0100

    fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,7 @@ const ObjectId = require('mongodb').ObjectID;
 
 module.exports = (app) => {
   app.post('/api/User/addFavouriteRestaurant', passport.authenticate('jwt', {session: false}), (req, res) => {
-    User.findOneAndUpdate(req.user._id, { $addToSet: { favouriteRestaurants: { _id: req.body._id } } }, function (err, product) {
-      debugger  
+    User.findOneAndUpdate(req.user._id, { $addToSet: { favouriteRestaurants: { _id: req.body._id } } }, function (err, product) { 
       if (err) console.log(err);
         res.send('Favourite restaurant added');
     });
